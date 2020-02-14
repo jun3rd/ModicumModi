7dfaa3846c307eb3815b83a7ba2f93f41463ab71
Jun Alvarado <jun3rd@gmail.com>
2020-02-14 01:40:25 -0800

    code fizzbuzz asTDD. Default TDD coding paradigm.

---
+++ js/fizzbuzz.js
@@ -0,0 +1,15 @@
+/*
+
+fizzbuzz Rules:
+    fizzbuzz is a function that takes a number as an argument
+    and then returns the number.
+
+    However:
+
+        - for multiples of 3 return "fizz" instead of the number.
+        - for multiples of 5 return "buzz" instead of the number.
+        - for multiples of 3 & 5 return "fizzbuzz".
+
+*/
+
+
