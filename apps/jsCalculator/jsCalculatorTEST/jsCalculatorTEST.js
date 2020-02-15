003780d174f7b3f76ebc5973c7f1e73583ddd1e6
Jun Alvarado <jun3rd@gmail.com>
2020-02-15 00:00:05 -0800

    Update directory structure, and added FCC apps.

---
+++ apps/jsCalculator/jsCalculatorTEST/jsCalculatorTEST.js
@@ -0,0 +1,21 @@
+/*
+
+refer to: butUncleBob.com/ArticleS.UncleBob.TheThreeRulesOfTdd
+*/
+
+/*
+
+You are not allowed to write any production code
+unless it is to make a failing unit test pass.
+
+You are not allowed to write any more of a unit test
+than is sufficient to fail;
+and compilation features are failures.
+
+You are not allowed to write any more production code
+than is sufficient to pass
+the ONE failing unit test.
+
+*/
+
+const expect = require('chai').expect;
